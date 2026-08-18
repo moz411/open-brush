85f7fdf84fea1a9ed6dd5af3c707f69d3acfa873
Thomas Dupouy <thomas.dupouy@moz42.net>
2026-08-18 15:14:13 +0200

    Enhance PassthroughManager with edge rendering options
    Added properties for edge rendering, edge color, and texture opacity to the PassthroughManager class. Updated Start method to initialize OVRPassthroughLayer with these properties.

--- Assets/Scripts/PassthroughManager.cs
+++ Assets/Scripts/PassthroughManager.cs
@@ -18,13 +18,58 @@ namespace TiltBrush
 {
     public class PassthroughManager : MonoBehaviour
     {
+#if OCULUS_SUPPORTED
+        [Header("Meta Passthrough")]
+        [SerializeField] private bool m_EnableEdgeRendering = true;
+        [SerializeField] private Color m_EdgeColor = Color.black;
+        [SerializeField, Range(0f, 1f)] private float m_TextureOpacity = 1f;
+
+        private OVRPassthroughLayer m_PassthroughLayer;
+#endif
+
         void Start()
         {
 #if OCULUS_SUPPORTED
-            var passthrough  = gameObject.AddComponent<OVRPassthroughLayer>();
-            passthrough.overlayType = OVROverlay.OverlayType.Underlay;
+            m_PassthroughLayer = gameObject.AddComponent<OVRPassthroughLayer>();
+            m_PassthroughLayer.overlayType = OVROverlay.OverlayType.Underlay;
+            m_PassthroughLayer.textureOpacity = m_TextureOpacity;
+            m_PassthroughLayer.edgeRenderingEnabled = m_EnableEdgeRendering;
+            m_PassthroughLayer.edgeColor = m_EdgeColor;
             App.VrSdk.m_OvrManager.shouldBoundaryVisibilityBeSuppressed = true;
 #endif // OCULUS_SUPPORTED
+        }
+
+        public void SetEdgeRendering(bool enabled)
+        {
+#if OCULUS_SUPPORTED
+            m_EnableEdgeRendering = enabled;
+            if (m_PassthroughLayer != null)
+            {
+                m_PassthroughLayer.edgeRenderingEnabled = enabled;
+            }
+#endif
+        }
+
+        public void SetEdgeColor(Color color)
+        {
+#if OCULUS_SUPPORTED
+            m_EdgeColor = color;
+            if (m_PassthroughLayer != null)
+            {
+                m_PassthroughLayer.edgeColor = color;
+            }
+#endif
+        }
+
+        public void SetTextureOpacity(float opacity)
+        {
+#if OCULUS_SUPPORTED
+            m_TextureOpacity = Mathf.Clamp01(opacity);
+            if (m_PassthroughLayer != null)
+            {
+                m_PassthroughLayer.textureOpacity = m_TextureOpacity;
+            }
+#endif
         }
 
         void OnDestroy()
